d4de93b541262d8c53d64aa3396073d396803b61
Rafael Terra <rafaelstjf@hotmail.com>
2021-06-01 09:48:03 -0300

    first gui commit

--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
-import tweepy, argparse, io, json, os
+import tweepy, argparse, io, json, os, sys
 from json import encoder
-
-def get_tweets(symptoms, keywords, query, lang):
+from PySide6 import QtCore, QtGui, QtUiTools, QtWidgets
+def get_tweets(api, symptoms, keywords, query, lang):
     max_count = 1000
     print("--------------------------------------")
     print("\t\tDownloading tweets")
@@ -70,7 +70,7 @@ def create_csv(in_filename, out_filename, symptoms, sep=';'):
             buffer = ""
             buffer = "class" + sep + "id" + sep + "text" + sep + "created_at" + sep + "language"
             for s in symptoms:
-                buffer += sep + s[0]
+                buffer += sep + s
             buffer+='\n'
             for json_str in json_list:
                 data = json.loads(json_str)
@@ -108,6 +108,62 @@ ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 
 
+def loadUiWidget(uifilename, parent=None):
+    loader = QtUiTools.QUiLoader()
+    file = QtCore.QFile("forms/gui.ui")
+    file.open(QtCore.QFile.ReadOnly)
+    myWidget = loader.load(file)
+    file.close()
+    return myWidget
+
+def array_to_string(array):
+    s = ""
+    for i in range(0, len(array)):
+        s+=array[i]
+        if(i < len(array)-1):
+            s+=','
+    return s
+
+def setup_window(api, mainWindow, configs):
+    mainWindow.out_field.setText(configs["output_file"])
+    mainWindow.key_field.setText(array_to_string(configs["keywords"]))
+    mainWindow.symp_field.setText(array_to_string(configs["symptoms"]))
+    mainWindow.black_field.setText(configs["blacklist"])
+    mainWindow.query_field.setText(configs["query"])
+    mainWindow.lang_field.setText(configs["language"])
+    mainWindow.download.clicked.connect(lambda: run_button(api, mainWindow, configs))
+    mainWindow.exit.clicked.connect(lambda: exit(0))
+
+def run_button(api, mainWindow, configs):
+    #disables editing
+    mainWindow.out_field.setReadOnly(True)
+    mainWindow.key_field.setReadOnly(True)
+    mainWindow.symp_field.setReadOnly(True)
+    mainWindow.black_field.setReadOnly(True)
+    mainWindow.query_field.setReadOnly(True)
+    mainWindow.lang_field.setReadOnly(True)
+    mainWindow.download.setFlat(True)
+    #Gets the parameters
+    configs["output_file"] = mainWindow.out_field.text()
+    configs["keywords"] = (mainWindow.key_field.text()).split(',')
+    configs["symptoms"] = (mainWindow.symp_field.text()).split(',')
+    configs["blacklist"] = mainWindow.black_field.text()
+    configs["query"] = mainWindow.query_field.text()
+    configs["language"] = mainWindow.lang_field.text()
+    tweets = get_tweets(api, configs["symptoms"], configs["keywords"], configs["query"] + " " + configs["blacklist"], configs["language"])
+    save_tweets(configs["output_file"], tweets)
+    csv_name = configs["output_file"].split('.')
+    if(mainWindow.convert.isChecked()):
+      create_csv(configs["output_file"], csv_name[0] + '.csv', configs["symptoms"])  
+    #enables editing again
+    mainWindow.out_field.setReadOnly(False)
+    mainWindow.key_field.setReadOnly(False)
+    mainWindow.symp_field.setReadOnly(False)
+    mainWindow.black_field.setReadOnly(False)
+    mainWindow.query_field.setReadOnly(False)
+    mainWindow.lang_field.setReadOnly(False)
+    mainWindow.download.setFlat(False)
+
 def main():
     #load and parse the arguments
     parser = argparse.ArgumentParser()
@@ -118,21 +174,22 @@ def main():
     #load configuration file
 
     try:
-        json_file = open(args.config_file)
+        json_file = open(args.config_file, encoding='utf8')
         configs = json.load(json_file)
     except:
         print("Error loading the configuration file! Exiting...")
         exit(1)
 
+   
     #Authenticate the account
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=set([503]))
-    #Parameters
-    tweets = get_tweets(configs["symptoms"], configs["keywords"], configs["query"] + " " + configs["blacklist"], configs["language"])
-    save_tweets(configs["output_file"], tweets)
-    if(args.convert_to_csv != ""):
-      create_csv(configs["output_file"], args.convert_to_csv, configs["symptoms"])  
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = loadUiWidget("forms/myform.ui")
+    setup_window(api, mainWindow, configs)
+    mainWindow.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
